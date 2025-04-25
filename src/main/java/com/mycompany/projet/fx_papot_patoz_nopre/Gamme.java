92071f03d8ec6dbecaeab356da8e76a718fe96ca
User2403-design <chloe.nopre24@gmail.com>
2025-04-25 11:05:18 +0200

    + classe personne

---
+++ src/main/java/com/mycompany/projet/fx_papot_patoz_nopre/Gamme.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projet.fx_papot_patoz_nopre;
+
+/**
+ *
+ * @author chloe
+ */
+public class Gamme {
+    
+}
